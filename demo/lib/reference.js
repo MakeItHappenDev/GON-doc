a1f0f313074209c34c183389aa6d76b1dce61578
PookMook <arthur.juchereau@gmail.com>
2019-11-28 17:43:08 -0500

    references

--- demo/lib/reference.js
+++ demo/lib/reference.js
@@ -2,7 +2,7 @@ function Reference(target,path = ["references"]){
   this.target = target
   this.path = path
   this.toJSON = () => {
-    return {type:"ref",target:`$${this.path.join('.')}.${this.target}`}
+    return `$${this.path.join('.')}.${this.target}`
   }
   this.toGON = () => {
     return `$${this.path.join('.')}.${this.target}`
--- demo/pages/references.js
+++ demo/pages/references.js
@@ -39,6 +39,12 @@ export default () => {
   return (
     <main className={styles.main}>
       <h1>Hello dev.to</h1>
+      <p>
+        <a href="https://github.com/MakeItHappenDev/GraphObjectNotation/blob/master/demo/pages/references.js" target="_blank" rel="noopener noreferrer">Here's the code</a>
+      </p>
+      <p>
+        <a href="https://dev.to/arthurbiensur/kind-of-getting-the-memory-address-of-a-javascript-object-2mnd" target="_blank" rel="noopener noreferrer">Here is a Post about this page</a>
+      </p>
       <pre>{`const user1 = {name:"Arthur"}
 const user2 = {name:"Bob"}
 
