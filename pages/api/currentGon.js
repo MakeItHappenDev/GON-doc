ea4298a243901e141c029cba7705d6f3e2697686
PookMook <arthur.juchereau@gmail.com>
2020-01-10 08:27:41 -0500

    beautify currentGon display

--- pages/api/currentGon.js
+++ pages/api/currentGon.js
@@ -3,7 +3,7 @@ import {stringify} from 'graph-object-notation'
 
 export default async (_,res) => {
 
-  const response = stringify(mutableDB)
+  const response = stringify(mutableDB,null,1)
 
   return res.end(response,null,1)
 }
