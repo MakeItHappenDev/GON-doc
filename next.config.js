ad501acb7c7f2df677d73cea840adf6f16cce28f
Arthur Juchereau <PookMook@users.noreply.github.com>
2020-01-29 09:29:38 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,5 @@ const withSass = require('@zeit/next-sass')
 
 
 module.exports =  withCSS(withSass({
-  cssModules: true,
-  env: {
-    GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
-    MONGODB: process.env.MONGODB
-  }
+  cssModules: true
 }))
